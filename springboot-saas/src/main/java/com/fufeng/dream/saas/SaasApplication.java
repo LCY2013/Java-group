8cedf96b3a49d020208647b15dadd5c7b0094a53
MagicLuo <15281205719@163.com>
2020-06-24 14:51:57 +0800

    saas 平台更新

---
+++ springboot-saas/src/main/java/com/fufeng/dream/saas/SaasApplication.java
@@ -0,0 +1,19 @@
+package com.fufeng.dream.saas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: Java-group
+ * @description: saas 平台启动入口
+ * @author: <a href="https://github.com/lcy2013">MagicLuo</a>
+ * @create: 2020-06-24 14:30
+ */
+@SpringBootApplication
+public class SaasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaasApplication.class,args);
+    }
+
+}
